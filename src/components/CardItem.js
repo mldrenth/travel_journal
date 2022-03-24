990637cf13e8c2c296b51c7dfa71f3c8199e230b
Marijke Drenth <drenth.marijke@googlemail.com>
2022-03-24 21:48:22 +0200

    Add styling

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,21 +4,25 @@ import {IoLocationSharp} from 'react-icons/io5'
 const CardItem = ({visit}) => {
     
     return (
+        <>
         <div className="card-div">
             <img src={visit.imageUrl} alt="sight"/>
             <div className="card-info">
                 <div className="country">
                     <IoLocationSharp className="location-icon"/>
-                    <p>{visit.location.toUpperCase()}</p>
+                    <p className="location">{visit.location.toUpperCase()}</p>
                     <a href={visit.googleMapsUrl}>View on Google Maps</a>
                 </div>
-                <h2>{visit.title}</h2>
-                <h4>{visit.startDate} - {visit.endDate}</h4>
-                <p>{visit.description}</p>
+                <h2 className="title">{visit.title}</h2>
+                <h4 className="date">{visit.startDate} - {visit.endDate}</h4>
+                <p className="description">{visit.description}</p>
 
             </div>
+            
 
         </div>
+        <hr/>
+        </>
     )
 }
 
